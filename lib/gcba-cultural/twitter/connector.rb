fa9917955fecec192e2c3cd430b0c909e4e745e1
jproyo <juanpablo.royo@gmail.com>
2012-09-11 16:43:01 -0300

    Hide tokens

--- lib/gcba-cultural/twitter/connector.rb
+++ lib/gcba-cultural/twitter/connector.rb
@@ -9,10 +9,10 @@ module Gcba
 
   		def initialize
 	    	@client = Twitter::Client.new(
-	    		:consumer_key => "1y20qLCUldIzzF9kN2rU1A",
-					:consumer_secret => "0LO3XI3HQU9OHPii0VMglIL0VH8nGuZ4qQNF90UNI",
-	  			:oauth_token => "75272994-rVRlOAVS2h6zaHZVNXPO2DWV4PMZu7nJA36elkcaS",
-	  			:oauth_token_secret => "WulUeanuQSKLv7yGmWxk4D1B7NQ10qVLaC7aW3uhZO8"
+	    		:consumer_key => "TO_BE_COMPLETED",
+					:consumer_secret => "TO_BE_COMPLETED",
+	  			:oauth_token => "TO_BE_COMPLETED",
+	  			:oauth_token_secret => "TO_BE_COMPLETED"
 			)
   		end
 
